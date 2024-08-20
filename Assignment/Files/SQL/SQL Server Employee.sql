4d4054544e367bb80423e8c05063d8a6a88823f8
krishnanand654 <krishnanand654@gmail.com>
2024-08-20 07:27:48 +0530

    SQL assignment

--- Assignment/Files/SQL/SQL Server Employee.sql
+++ Assignment/Files/SQL/SQL Server Employee.sql
@@ -233,9 +233,225 @@ GROUP BY Department;
 
 -- m) Find the if there is any relation between Attrition Rate and Marital Status of Employee.
 
+SELECT
+    Marital_Status,
+    SUM(CASE WHEN Attrition = 'Yes' THEN 1 ELSE 0 END) AS Attrition_Yes,
+    SUM(CASE WHEN Attrition = 'No' THEN 1 ELSE 0 END) AS Attrition_No
+FROM
+    EmployeeData
+GROUP BY
+    Marital_Status
+ORDER BY Attrition_Yes;
+
 -- n) Show the Department with Highest Attrition Rate (Percentage)
+SELECT
+	Department,
+	SUM(
+	CASE
+		WHEN Attrition = 'Yes' THEN 1 ELSE 0
+	END) AS attrition_count_yes,
+	SUM(
+	CASE
+		WHEN Attrition = 'Yes' THEN 1 ELSE 0
+	END)*100/COUNT(Attrition) AS Attrition_Percentage
+FROM
+	EmployeeData
+GROUP BY 
+	Department;
+
 -- o) Calculate the moving average of monthly income over the past 3 employees for each job  role.
--- p) Identify employees with outliers in monthly income within each job role. [ Condition : 
--- Monthly_Income < Q1 - (Q3 - Q1) * 1.5 OR Monthly_Income > Q3 + (Q3 - Q1) ]
+SELECT
+    Job_Role,
+    Employee_Number,
+    Monthly_Income,
+    AVG(Monthly_Income) OVER (PARTITION BY Job_Role ORDER BY Employee_Number ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) AS Moving_Avg_Income
+FROM
+    EmployeeData
+ORDER BY
+    Job_Role, Employee_Number;
+
+-- p) Identify employees with outliers in monthly income within each job role. 
+-- [ Condition : Monthly_Income < Q1 - (Q3 - Q1) * 1.5 OR Monthly_Income > Q3 + (Q3 - Q1) ]
+
+WITH Quartiles AS (
+    SELECT
+        Job_Role,
+        Monthly_Income,
+        NTILE(4) OVER (PARTITION BY Job_Role ORDER BY Monthly_Income) AS Quartile
+    FROM
+        EmployeeData
+),
+Q1ANDQ3 AS (
+    SELECT
+        Job_Role,
+        MIN(CASE WHEN Quartile = 1 THEN Monthly_Income END) AS Q1,
+        MIN(CASE WHEN Quartile = 3 THEN Monthly_Income END) AS Q3
+    FROM
+        Quartiles
+    GROUP BY
+        Job_Role
+)
+SELECT
+    e.Job_Role,
+    e.Employee_Number,
+    e.Monthly_Income,
+    Q1,
+    Q3,
+    (Q3 - Q1) AS IQR,
+    (Q1 - (Q3 - Q1) * 1.5) AS Left_Outlier,
+    (Q3 + (Q3 - Q1)) AS Right_Outlier
+FROM
+    EmployeeData e
+JOIN
+    Q1ANDQ3  ON e.Job_Role = Q1ANDQ3.Job_Role
+WHERE
+    e.Monthly_Income < Q1 - (Q3 - Q1) * 1.5
+    OR e.Monthly_Income > Q3 + (Q3 - Q1)
+ORDER BY
+    e.Job_Role, e.Employee_Number;
+
 -- q) Gender distribution within each job role, show each job role with its gender domination.  [Male_Domination or Female_Domination] 
+WITH GenderCTE AS(
+	SELECT
+		Job_Role,
+		Gender,
+		COUNT(Gender) AS Gender_count,
+		DENSE_RANK() OVER(PARTITION BY Job_Role ORDER BY Gender) AS gender_rank
+	FROM
+		EmployeeData
+	GROUP BY 
+		Job_Role, Gender
+)
+SELECT 
+	Job_Role,
+
+	CASE
+		WHEN gender_rank = 2 THEN 'Male_Domination'
+		ELSE 'Female_Domination'
+	END AS Domination
+FROM 
+	GenderCTE
+WHERE 
+	gender_rank = 2
+ORDER BY 
+	Job_Role, Gender ASC;
+
 -- r) Percent rank of employees based on training times last year
+
+SELECT
+    Employee_Number,
+    Training_Times_Last_Year,
+    PERCENT_RANK() OVER (ORDER BY Training_Times_Last_Year) AS Percent_Rank
+FROM
+    EmployeeData
+ORDER BY
+    Percent_Rank;
+
+-- s) Divide employees into 5 groups based on training times last year [Use NTILE ()]
+WITH groupCTE AS(
+	SELECT
+		Employee_Number,
+		Training_Times_Last_Year,
+		NTILE(5) OVER(ORDER BY Training_Times_Last_Year) AS group_tile
+	FROM
+		EmployeeData
+)
+SELECT
+	Employee_Number,
+	Training_Times_Last_Year,
+	CASE
+		WHEN group_tile = 1 THEN 'group 1'
+		WHEN group_tile = 2 THEN 'group 2'
+		WHEN group_tile = 3 THEN 'group 3'
+		WHEN group_tile = 4 THEN 'group 4'
+		ELSE 'group 5'
+	END AS Employee_Training_Group
+FROM
+	groupCTE;
+
+--t) Categorize employees based on training times last year as - Frequent Trainee, Moderate  Trainee, Infrequent Trainee
+
+WITH TrainingStats AS (
+    SELECT
+        Employee_Number,
+        Training_Times_Last_Year,
+        PERCENT_RANK() OVER (ORDER BY Training_Times_Last_Year ) AS Percent_Rank
+    FROM
+        EmployeeData
+)
+SELECT
+    Employee_Number,
+    Training_Times_Last_Year,
+    CASE
+        WHEN Percent_Rank >= 0.75 THEN 'Frequent Trainee'
+        WHEN Percent_Rank >= 0.50 THEN 'Moderate Trainee'
+        ELSE 'Infrequent Trainee'
+    END AS Trainee_Category
+FROM
+    TrainingStats
+ORDER BY
+    Training_Times_Last_Year;
+
+-- u) Categorize employees as 'High', 'Medium', or 'Low' performers based on their performance rating, using a CASE WHEN statement.
+
+SELECT 	
+	PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY Performance_Rating) OVER () AS _
+FROM	
+	EmployeeData
+
+SELECT
+	Performance_Rating,
+	CASE
+		WHEN Performance_Rating >3 THEN 'High'
+		WHEN Performance_Rating <3 THEN 'low'
+		ELSE 'Medium'
+	END AS Category
+FROM
+	EmployeeData
+
+-- v) Use a CASE WHEN statement to categorize employees into 'Poor', 'Fair', 'Good', or 'Excellent'  work-life balance based on their work-life balance score.
+SELECT
+	DISTINCT
+		Work_Life_Balance
+FROM
+	EmployeeData
+
+SELECT
+	Work_Life_Balance,
+	CASE
+		WHEN Work_Life_Balance > 4 THEN 'Excellent'
+		WHEN Work_Life_Balance > 3 THEN 'Good'
+		WHEN Work_Life_Balance > 2 THEN 'Fair'
+		ELSE 'Poor'
+	END AS wlb_Status
+FROM
+	EmployeeData;
+
+-- w) Group employees into 3 groups based on their stock option level using the [NTILE] function.
+SELECT
+	Stock_Option_Level,
+	CASE
+		WHEN SOL_dist = 1 THEN 'group 1'
+		WHEN SOL_dist = 2 THEN 'group 2'
+		ELSE 'group 3'
+	END As sol_groups
+FROM(
+SELECT
+	Stock_Option_Level,
+	NTILE(3) OVER(ORDER BY Stock_Option_Level) AS SOL_dist
+FROM
+	EmployeeData) AS _
+
+-- x) Find key reasons for Attrition in Company
+SELECT
+	Department,
+	Job_Role,
+	AVG(Distance_From_Home) AS avg_distance,
+	AVG(Job_Satisfaction) AS avg_job_satisfaction,
+	AVG(Monthly_Income) AS avg_monthly_income,
+	AVG(Environment_Satisfaction) AS avg_env_satisfaction,
+	SUM(CASE WHEN Attrition = 'Yes' THEN 1 ELSE 0 END) AS Attrition_Yes_count,
+	(SUM(CASE WHEN Attrition = 'Yes' THEN 1 ELSE 0 END)*100.0/COUNT(Attrition)) AS Attrition_Yes_Percentage
+FROM
+	EmployeeData
+GROUP BY Department, Job_Role
